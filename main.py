e84e8abb63449126f639ca592e0449a0199ff87c
Ben W. <66909546+d4nt3-fpv@users.noreply.github.com>
2024-09-21 13:04:07 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from ttkbootstrap.constants import *
 from ttkbootstrap.scrolled import ScrolledText
 from tkinter import filedialog
 
-
 import serial.tools.list_ports
+
+import vlc
+
+
 
 ports = serial.tools.list_ports.comports()
 
@@ -16,8 +19,19 @@ for onePort in ports:
     print(str(onePort))
 
 
+def connect_to_arduino(comport, baudrate=9600):
+    global ser
+    ser = serial.Serial(comport, baudrate)
+
+def send_string_to_arduino(stringtosend):
+    ser.write(stringtosend.encode())
+
+def close_connection_to_arduino():
+    ser.close()
+
 
 root = ttk.Window(title="Johannes Feuerwerk", themename="darkly",resizable=(False,False))
+
 
 headline = ttk.Label(root, text="Feuerwerk Steuerung", style="danger.TLabel", font=('Helvetica', 35))
 headline.grid(row=1, column=1, padx=10, pady=10)
@@ -26,6 +40,14 @@ selectedPort = StringVar(root)
 
 videofilepath = StringVar(root)
 timecodefilepath = StringVar(root)
+
+
+def connectbtnclick():
+    sel_port = str(str(selectedPort.get()).split(" ")[0]).rstrip('\n').rstrip('\r')
+    print("------")
+    print(sel_port)
+    connect_to_arduino(sel_port)
+
 
 def load_video_file():
     filename = filedialog.askopenfilename()
@@ -39,6 +61,13 @@ def load_timecode_file():
     timecodefilepath.set(filename)
 
 
+def start_show_btn_click():
+    
+    media = vlc.MediaPlayer(videofilepath.get())
+    media.play()
+
+
+
 ### Connect to Arduino frame
 
 Connect_to_Arduino_frame = ttk.Labelframe(root, text='Connect to Arduino', style='info.TLabelframe')
@@ -47,7 +76,7 @@ Connect_to_Arduino_frame.grid(row=2, column=1, padx=10, pady=15)
 select_Port_combobox = ttk.Combobox(Connect_to_Arduino_frame, values=portsList, state='readonly', textvariable=selectedPort)
 select_Port_combobox.grid(row=1, column=1, padx=5, pady=10)
 
-connect_to_arduino_button = ttk.Button(Connect_to_Arduino_frame, bootstyle="primary", text="Connect")
+connect_to_arduino_button = ttk.Button(Connect_to_Arduino_frame, bootstyle="primary", text="Connect", command=connectbtnclick)
 connect_to_arduino_button.grid(row=1, column=2, padx=5, pady=10)
 
 connection_status_lbl = ttk.Label(Connect_to_Arduino_frame, text="Connection Status: ")
@@ -88,7 +117,7 @@ timecode_file_path_browse_btn.grid(row=2, column=3, padx=5, pady=5)
 show_control_frame = ttk.Labelframe(root, text='Show Control', style='info.TLabelframe')
 show_control_frame.grid(row=4, column=1, padx=10, pady=15)
 
-Start_show_btn = ttk.Button(show_control_frame, text="Start show", bootstyle="success")
+Start_show_btn = ttk.Button(show_control_frame, text="Start show", bootstyle="success", command=start_show_btn_click)
 Start_show_btn.grid(row=1, column=1, padx=5, pady=5)
 
 stop_show_btn = ttk.Button(show_control_frame, text="Stop show", bootstyle="danger")
